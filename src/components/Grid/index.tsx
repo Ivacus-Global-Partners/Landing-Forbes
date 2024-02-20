564bd456dd39837c586ebc920caf68333b450ce0
unknown <Amiras@IVLT04.ivacus.local>
2024-02-20 16:36:21 +0100

    forbes

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -5,10 +5,10 @@ interface FlipCardProps {
   title: string;
   description: string;
   imageSrc: string;
-  date: string;
-  modality: string;
-  modules: string;
-  duration: string;
+  date?: string;
+  modality?: string;
+  modules?: string;
+  duration?: string;
   link: string;
 }
 
@@ -24,11 +24,12 @@ function FlipCard({ title, description, imageSrc, date, modality, modules, durat
           </div>
         </div>
         <div className="flip-card-back">
-          <ul>
-            <li><b>Fecha</b>: {date}</li>
-            <li><b>Modalidad</b>: {modality}</li>
-            <li><b>Módulos</b>: {modules}</li>
-            <li><b>Duración</b>: {duration}</li>
+        <ul>
+            {/* Renderizar los campos solo si tienen contenido */}
+            {date && date.trim() !== '' && <li><b>Fecha</b>: {date}</li>}
+            {modality && modality.trim() !== '' && <li><b>Modalidad</b>: {modality}</li>}
+            {modules && modules.trim() !== '' && <li><b>Módulos</b>: {modules}</li>}
+            {duration && duration.trim() !== '' && <li><b>Duración</b>: {duration}</li>}
           </ul>
           <a href={link}>Más Info</a>
         </div>
@@ -39,6 +40,8 @@ function FlipCard({ title, description, imageSrc, date, modality, modules, durat
 
 function Grid() {
   return (
+    <div>
+     <h2 className="grid-title">Executive Programs</h2>
     <div className="card-container">
       <div className="card">
         <FlipCard
@@ -76,6 +79,39 @@ function Grid() {
           link="https://forbes.es/forbes-sagardoy-business-school/el-poder-de-la-diversidad/"
         />
       </div>
+      <div className="card">
+        <FlipCard
+          title="IA"
+          description="Transformando negocios e industrias"
+          imageSrc="https://forbes.es/wp-content/uploads/2024/01/3-1-scaled.jpg"
+          date="Mayo-Junio (Jueves)"
+          modality="Presencial o virtual"
+          modules="3 módulos"
+          duration="6 sesiones"
+          link="https://forbes.es/forbes-sagardoy-business-school/el-poder-de-la-diversidad/"
+        />
+      </div>
+      <div className="card">
+        <FlipCard
+          title="Sostenibilidad corporativa"
+          description="Claves para el futuro"
+          imageSrc="https://forbes.es/wp-content/uploads/2024/01/3-1-scaled.jpg"
+          date="Mayo-Junio (Jueves)"
+          modality="Presencial o virtual"
+          modules="8 módulos"
+          duration="8 sesiones"
+          link="https://forbes.es/forbes-sagardoy-business-school/el-poder-de-la-diversidad/"
+        />
+      </div>
+      <div className="card">
+        <FlipCard
+          title="Forbes Sagardoy: In Company"
+          description="Programas a medida diseñados para tu empresa"
+          imageSrc="https://forbes.es/wp-content/uploads/2024/01/3-1-scaled.jpg"
+         link="https://forbes.es/forbes-sagardoy-business-school/el-poder-de-la-diversidad/"
+        />
+      </div>
+    </div>
     </div>
   );
 }
