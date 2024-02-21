955a239ab47966602e6eedb0a0dee0d1ddcc3021
unknown <Amiras@IVLT04.ivacus.local>
2024-02-21 01:07:49 +0100

    cambios

--- src/components/FinalPart/index.tsx
+++ src/components/FinalPart/index.tsx
@@ -9,14 +9,23 @@ function AdditionalContent() {
         <div className="content">
           <div className="wp-block-columns columnas-porque-elearning is-layout-flex wp-container-core-columns-layout-4 wp-block-columns-is-layout-flex">
             <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
-              <ol>
-                <li><strong>Promover la excelencia empresarial</strong><br /> Actuar como un socio estratégico, proporcionando orientación, conocimiento actualizado, herramientas y apoyo para impulsar el crecimiento empresarial de nuestros partners.</li>
-                <br />
-                <li><strong>Desarrollo continuo del talento</strong><br /> Guiar y acompañar a las empresas en el desarrollo constante de sus equipos, dotándoles de las herramientas necesarias para crecer en un contexto en el cual el cambio es una constante.</li>
-                <br />
-                <li><strong>Respuesta a necesidades específicas y transversales</strong><br /> Entender las necesidades de cada empresa, ofreciendo soluciones formativas en formatos en abierto con el fin de impulsar una comunidad de expertos y en formatos in-company ofreciendo soluciones formativas personalizadas.</li>
-                <br />
-                <li><strong>Desarrollo de habilidades clave</strong><br /> Desarrollar competencias clave como el pensamiento estratégico, sistémico y la capacidad de innovación para sentar las bases para un crecimiento sostenido.</li>
+              <ol className="custom-ol">
+                <li>
+                  <strong>Promover la excelencia empresarial</strong>
+                  <p>Actuar como un socio estratégico, proporcionando orientación, conocimiento actualizado, herramientas y apoyo para impulsar el crecimiento empresarial de nuestros partners.</p>
+                </li>
+                <li>
+                  <strong>Desarrollo continuo del talento</strong>
+                  <p>Guiar y acompañar a las empresas en el desarrollo constante de sus equipos, dotándoles de las herramientas necesarias para crecer en un contexto en el cual el cambio es una constante.</p>
+                </li>
+                <li>
+                  <strong>Respuesta a necesidades específicas y transversales</strong>
+                  <p>Entender las necesidades de cada empresa, ofreciendo soluciones formativas en formatos en abierto con el fin de impulsar una comunidad de expertos y en formatos in-company ofreciendo soluciones formativas personalizadas.</p>
+                </li>
+                <li>
+                  <strong>Desarrollo de habilidades clave</strong>
+                  <p>Desarrollar competencias clave como el pensamiento estratégico, sistémico y la capacidad de innovación para sentar las bases para un crecimiento sostenido.</p>
+                </li>
               </ol>
             </div>
           </div>
